315abdd906a0113905227400d3f0a23c3d401dd4
krharsh18032 <Harshvardhan.bcastudent22.18032@cimage.in>
2024-07-09 13:12:25 +0530

    new app

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import {AppRegistry} from 'react-native';
 // import Grid from './Grid';
 // import  ComponentList from './ComponentList';
 // import ClassComponent from './ClassComponent';
-import Calculator from './Calculator';
+// import Calculator from './Calculator';
+import App from './App';
+
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () =>Calculator);
+AppRegistry.registerComponent(appName, () =>App);

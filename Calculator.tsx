f8229a8e317623b34f5457cf84396458d6122a21
krharsh18032 <Harshvardhan.bcastudent22.18032@cimage.in>
2024-07-09 10:41:42 +0530

    removed calculator logic

--- Calculator.tsx
+++ Calculator.tsx
@@ -2,50 +2,7 @@ import React, { useState } from "react";
 
 import { View,Text,StyleSheet,TextInput,Keyboard ,TouchableHighlight,useColorScheme,Dimensions} from "react-native";
 
-const calculate = (input:String) => {
-    const operators = ['+', '-', '*', '/'];
-    const inputArray = input.split(' ');
-    const num1 = Number.parseInt(inputArray[0]);
-    const operator = inputArray[1];
-    const num2 = Number.parseInt(inputArray[2]);
-  
-    if (isNaN(num1) && isNaN(num2)) {
-        return 'Both INs';
-      }
 
-      if (isNaN(num1) ) {
-        return 'invalid num1';
-      }
-
-      if (isNaN(num2) ) {
-        return 'invalid num2';
-      }
-
-    if (isNaN(num1) || isNaN(num2)) {
-      return 'Either Ins';
-    }
-  
-    if (!operators.includes(operator)) {
-      return 'Invalid operator';
-    }
-  
-    switch (operator) {
-      case '+':
-        return (num1 + num2).toString();
-      case '-':
-        return (num1 - num2).toString();
-      case '*':
-        return (num1 * num2).toString();
-      case '/':
-        if (num2 === 0) {
-          return 'Cannot divide by zero';
-        }
-        return (num1 / num2).toString();
-      default:
-        return 'Invalid input';
-    }
-  };
-  
   
 function Calculator(){
 const [val,setVal]=useState("")

cd8f0cfe6440cafb9e45f4c0a3ccbd303e0ac84c
krharsh18032 <Harshvardhan.bcastudent22.18032@cimage.in>
2024-07-09 10:36:27 +0530

    Shifted the calculator logic in a seperate file

--- CalculatorLogic.js
+++ CalculatorLogic.js
@@ -1,11 +1,45 @@
 import React from "react";
 
-function CalculatorLogic(){
-   const sum = (a,b)=>{
+export default function Calculate(input:String) => {
+    const operators = ['+', '-', '*', '/'];
+    const inputArray = input.split(' ');
+    const num1 = Number.parseInt(inputArray[0]);
+    const operator = inputArray[1];
+    const num2 = Number.parseInt(inputArray[2]);
+  
+    if (isNaN(num1) && isNaN(num2)) {
+        return 'Both INs';
+      }
 
-        return a+b;
-        
+      if (isNaN(num1) ) {
+        return 'invalid num1';
+      }
+
+      if (isNaN(num2) ) {
+        return 'invalid num2';
+      }
+
+    if (isNaN(num1) || isNaN(num2)) {
+      return 'Either Ins';
     }
-}
-
-export default CalculatorLogic;
+  
+    if (!operators.includes(operator)) {
+      return 'Invalid operator';
+    }
+  
+    switch (operator) {
+      case '+':
+        return (num1 + num2).toString();
+      case '-':
+        return (num1 - num2).toString();
+      case '*':
+        return (num1 * num2).toString();
+      case '/':
+        if (num2 === 0) {
+          return 'Cannot divide by zero';
+        }
+        return (num1 / num2).toString();
+      default:
+        return 'Invalid input';
+    }
+  };

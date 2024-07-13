4e5a4c53c0f1d81d20e8ae091488c82db9976882
krharsh18032 <Harshvardhan.bcastudent22.18032@cimage.in>
2024-07-13 12:43:13 +0530

    registry cahnge

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ import {AppRegistry} from 'react-native';
 // import Grid from './Grid';
 // import  ComponentList from './ComponentList';
 // import ClassComponent from './ClassComponent';
-// import Calculator from './Calculator';
-import App from './App';
+import Calculator from './Calculator';
+// import App from './App';
 
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () =>App);
+// AppRegistry.registerComponent(appName, () =>App);
+AppRegistry.registerComponent(appName, () =>Calculator);
+
